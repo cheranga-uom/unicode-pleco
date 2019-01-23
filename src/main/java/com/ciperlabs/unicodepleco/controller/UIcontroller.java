507bb434e3492f892974cc894d81d37e476b1ae3
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2019-01-23 23:46:46 +0530

    About us, issue front end integrated

--- src/main/java/com/ciperlabs/unicodepleco/controller/UIcontroller.java
+++ src/main/java/com/ciperlabs/unicodepleco/controller/UIcontroller.java
@@ -18,6 +18,15 @@ public class UIcontroller {
         return "user/index";
     }
 
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "user/about";
+    }
+
+    @GetMapping("/report_issue")
+    public String reportIssue(Model model) {
+        return "user/reportIssue";
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
