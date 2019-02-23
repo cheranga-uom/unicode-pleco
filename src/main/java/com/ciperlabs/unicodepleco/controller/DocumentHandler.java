755b513a8831663cfaeb9430220633104debaa6e
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2019-02-23 18:37:14 +0530

    Fixed absolute path problem, Fixed Test dependency issue

--- src/main/java/com/ciperlabs/unicodepleco/controller/DocumentHandler.java
+++ src/main/java/com/ciperlabs/unicodepleco/controller/DocumentHandler.java
@@ -5,6 +5,8 @@ import com.ciperlabs.unicodepleco.documentHandler.word.WDXToUnicode;
 import com.ciperlabs.unicodepleco.model.FileType;
 import com.ciperlabs.unicodepleco.service.storage.StorageService;
 import com.ciperlabs.unicodepleco.service.storage.StoredFile;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.poi.POIXMLDocument;
 import org.apache.poi.POIXMLException;
 import org.apache.poi.util.IOUtils;
@@ -22,18 +24,11 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.mock.web.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /*
 This class will handle the received stored file objects to identify the file type and convert it accordingly
@@ -69,7 +64,7 @@ public class DocumentHandler {
 
         StoredFile convertedDocument;
 
-        if(fileType.equals("officeXML")){
+        if (fileType.equals("officeXML")) {
             try {
                 logger.info("Trying to Convert as a docx  file");
                 convertedDocument = tryDocx(multipartFile);
@@ -77,15 +72,12 @@ public class DocumentHandler {
                 logger.info("DOCX Conversion attempt failed. Trying as an excel File");
                 convertedDocument = tryExcel(multipartFile);
             }
-        }
-        else if(fileType.equals("PDF")){
+        } else if (fileType.equals("PDF")) {
 
             logger.info("Trying ti convert as a PDF file");
             convertedDocument = tryPDF(multipartFile);
 
-        }
-        else convertedDocument = null;
-
+        } else convertedDocument = null;
 
 
         return convertedDocument;
@@ -130,7 +122,7 @@ public class DocumentHandler {
         return convertedDocument;
     }
 
-    private StoredFile tryPDF(MultipartFile multipartFile){
+    private StoredFile tryPDF(MultipartFile multipartFile) {
         StoredFile convertedDocument = null;
 
 
@@ -141,34 +133,56 @@ public class DocumentHandler {
 //            e.printStackTrace();
 //        }
         String uri = "http://localhost:5000/api/converter";
-        logger.info("Sending PDF file to convert  : "+uri);
+        logger.info("Sending PDF file to convert  : " + uri);
         RestTemplate restTemplate = new RestTemplate();
         StoredFile storedPDF = storageService.store(multipartFile, pdfToWordPdfLocation);
-        logger.info("Converting PDF to word.. : "+ storedPDF.getPath());
+
+        String absoluteDirectoryPath = storageService.load("").toAbsolutePath().toString();
+        String absolutePDFPath = absoluteDirectoryPath + "/" + storedPDF.getPath();
+        logger.info("Abs Path" + absoluteDirectoryPath);
+        logger.info("Abs PDF File path PDF to word.. : " + absolutePDFPath);
 
         MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-        map.add("pdfFilePath", storedPDF.getPath());
+        map.add("pdfFilePath", absoluteDirectoryPath);
 
-        String docxFilePath = pdfToWordDocxLocation+multipartFile.getOriginalFilename()+".docx";
+        String docxLoadingFilePath = pdfToWordDocxLocation + storedPDF.getFileName() + ".docx";
+        String absoluteDocxPath = absoluteDirectoryPath + "/" + docxLoadingFilePath;
 
-        logger.info("Converting PDF docx filePath .. : " + docxFilePath);
-        map.add("docxFilePath", docxFilePath);
+        logger.info("pdf to docx Absolute saving location : " + absoluteDocxPath);
 
-        //TODO form data
+        map.add("docxFilePath", absoluteDocxPath);
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
-        ResponseEntity<String> response = restTemplate.postForEntity( uri, request , String.class );
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, request, String.class);
         logger.info(response.toString());
 
         try {
-            File docxFile = storageService.loadAsResource(docxFilePath).getFile();
-            FileInputStream docxInputStream = new FileInputStream(docxFile);
-            MultipartFile multipartFile1 = new MockMultipartFile(docxFile.getName(),
-                    docxFile.getName(), "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                    IOUtils.toByteArray(docxInputStream));
+            logger.info("Trying to load Converted PDF docx filePath .. : " + docxLoadingFilePath);
+
+            File docxFile = storageService.loadAsResource(docxLoadingFilePath).getFile();
+
+//            FileInputStream docxInputStream = new FileInputStream(docxFile);
+//            MultipartFile multipartFile1 = new MockMultipartFile(docxFile.getName(),
+//                    docxFile.getName(), "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+//                    IOUtils.toByteArray(docxInputStream));
+
+            FileItem fileItem = new DiskFileItem(docxFile.getName(), Files.probeContentType(docxFile.toPath()), false, docxFile.getName(), (int) docxFile.length(), docxFile.getParentFile());
+
+            try {
+                InputStream input = new FileInputStream(docxFile);
+                OutputStream os = fileItem.getOutputStream();
+                IOUtils.copy(input, os);
+                // Or faster..
+                // IOUtils.copy(new FileInputStream(file), fileItem.getOutputStream());
+            } catch (IOException ex) {
+                // do something.
+            }
+
+            MultipartFile multipartFile1 = new CommonsMultipartFile(fileItem);
 
             convertedDocument = tryDocx((multipartFile1));
             convertedDocument.setFileType(FileType.PDF);
@@ -183,7 +197,7 @@ public class DocumentHandler {
     public StoredFile saveFile(String convertedFileLocation, MultipartFile multipartFile, POIXMLDocument convertedFile) {
 
         StoredFile convertedDocument = new StoredFile();
-        String rootConvertedFileLocation = rootDocumentDirectory+convertedFileLocation;
+        String rootConvertedFileLocation = rootDocumentDirectory + convertedFileLocation;
         try {
             File directory = new File(convertedFileLocation);
             logger.info("Creating Uplo  ad directory if not exist : " + convertedFileLocation + " : " + directory.mkdirs());
