392eb20d7b3b5674c096325114c5a4961c91e2cf
sudeepa14 <sudeepa.14@cse.mrt.ac.lk>
2019-03-01 15:10:10 +0530

    added MI damindu support

---
+++ src/main/java/com/ciperlabs/unicodepleco/documentHandler/ConvertionEngine/LegacyToUnicodeFontMappings/Sinhala/MiDamindu2000.java
@@ -0,0 +1,428 @@
+package com.ciperlabs.unicodepleco.documentHandler.ConvertionEngine.LegacyToUnicodeFontMappings.Sinhala;
+
+public class MiDamindu2000 {
+
+    public static String convert(String text) {
+
+        text = text.replaceAll("ƒœ±±±", "ශ්‍රී");
+        text = text.replaceAll("¨¨q²", "ත්‍රෛ");
+        text = text.replaceAll("¨w±°", "ප්‍රෞ");
+
+        text = text.replaceAll("¨¨ƒ", "ශෛ");
+        text = text.replaceAll("¨¨e", "චෛ");
+        text = text.replaceAll("¨¨g", "ජෛ");
+        text = text.replaceAll("¨¨u", "නෛ");
+        text = text.replaceAll("¨¨_", "කෛ");
+        text = text.replaceAll("¨¨\\{", "මෛ");
+        text = text.replaceAll("¨¨w", "පෛ");
+        text = text.replaceAll("¨¨q", "තෛ");
+        text = text.replaceAll("¨¨t", "ධෛ");
+        text = text.replaceAll("¨¨‚", "වෛ");
+        text = text.replaceAll("¨¨…", "සෛ");
+        text = text.replaceAll("¨¨x", "එෛ");    //issue fixeds
+
+        text = text.replaceAll("¨„µ", "ෂ්‍යෝ");
+        text = text.replaceAll("¨nµ", "ඪ්‍යෝ");
+        text = text.replaceAll("¨bµ", "ඝ්‍යෝ");
+        text = text.replaceAll("¨`µ", "ඛ්‍යෝ");
+        text = text.replaceAll("¨‡µ", "ළ්‍යෝ");
+        text = text.replaceAll("¨xµ", "ඵ්‍යෝ");
+        text = text.replaceAll("¨lµ", "ඨ්‍යෝ"); //recheck
+        text = text.replaceAll("¨ƒµ", "ශ්‍යෝ");
+        text = text.replaceAll("¬„‹¨}ýŒ", "ක්‍ෂ්යෝ");
+        text = text.replaceAll("¨yµ", "බ්‍යෝ");
+        text = text.replaceAll("¨eµ", "ච්‍යෝ");
+        text = text.replaceAll("¨mµ", "ඩ්‍යෝ");
+        text = text.replaceAll("¨‰µ", "ෆ්‍යෝ");
+        text = text.replaceAll("¨aµ", "ග්‍යෝ");
+        text = text.replaceAll("¨gµ", "ජ්‍යෝ");
+        text = text.replaceAll("¨_µ", "ක්‍යෝ");
+        text = text.replaceAll("¨f°", "ඡෞ");
+        text = text.replaceAll("¨ƒ°", "ශෞ");
+        text = text.replaceAll("¨y°", "බෞ");
+        text = text.replaceAll("¨e°", "චෞ");
+        text = text.replaceAll("¨m°", "ඩෞ");
+        text = text.replaceAll("¨‰°", "ෆෞ");
+        text = text.replaceAll("¨a°", "ගෞ");
+        text = text.replaceAll("¨g°", "ජෞ");
+        text = text.replaceAll("¨_°", "කෞ");
+        text = text.replaceAll("¨°", "ලෞ");
+        text = text.replaceAll("¨\\{°", "මෞ");
+        text = text.replaceAll("¨u°", "නෞ");
+        text = text.replaceAll("¨w°", "පෞ");
+        text = text.replaceAll("¨s°", "දෞ");
+        text = text.replaceAll("¨~°", "රෞ");
+        text = text.replaceAll("¨…°", "සෞ");
+        text = text.replaceAll("¨k°", "ටෞ");
+        text = text.replaceAll("¨q°", "තෞ");
+        text = text.replaceAll("¨z°", "භෞ");
+        text = text.replaceAll("ª¬s°", "ඤෞ");
+
+        text = text.replaceAll("¨„", "ෂෝ");
+        text = text.replaceAll("¨|", "ඹෝ");
+        text = text.replaceAll("¨f", "ඡෝ");
+        text = text.replaceAll("¨n", "ඪෝ");
+        text = text.replaceAll("¨b", "ඝෝ");
+        text = text.replaceAll("¨`", "ඛෝ");
+        text = text.replaceAll("¨‡", "ළෝ");
+        text = text.replaceAll("¨", "ලෝ");
+        text = text.replaceAll("¨ª†", "ඟෝ");
+        text = text.replaceAll("¨o", "ණෝ");
+        text = text.replaceAll("¨x", "ඵෝ");
+        text = text.replaceAll("¨l", "ඨෝ");
+        text = text.replaceAll("¨p", "ඬෝ");
+        text = text.replaceAll("¨ƒ", "ශෝ");
+        text = text.replaceAll("¨j", "ඥෝ");
+        text = text.replaceAll("¨v", "ඳෝ");
+        text = text.replaceAll("¨„", "ෂෝ");
+        text = text.replaceAll("¨y", "බෝ");
+        text = text.replaceAll("¨e", "චෝ");
+        text = text.replaceAll("¨m", "ඩෝ");
+        text = text.replaceAll("¨‰", "ෆෝ");
+        text = text.replaceAll("¨a", "ගෝ");
+        text = text.replaceAll("¨†", "හෝ");
+        text = text.replaceAll("¨g", "ජෝ");
+        text = text.replaceAll("¨_", "කෝ");
+        text = text.replaceAll("¨‡", "ලෝ");
+        text = text.replaceAll("¨\\{", "මෝ");
+        text = text.replaceAll("¨u", "නෝ");
+        text = text.replaceAll("¨w", "පෝ");
+        text = text.replaceAll("¨s", "දෝ");
+        text = text.replaceAll("¨~", "රෝ");
+        text = text.replaceAll("¨…", "සෝ");
+        text = text.replaceAll("¨k", "ටෝ");
+        text = text.replaceAll("¨‚", "වෝ");
+        text = text.replaceAll("¨q", "තෝ");
+        text = text.replaceAll("¨z", "භෝ");
+        text = text.replaceAll("¨}", "යෝ");
+        text = text.replaceAll("¨i", "ඤෝ");
+        text = text.replaceAll("¨t", "ධෝ");
+        text = text.replaceAll("¨r", "ථෝ");
+
+        text = text.replaceAll("¨„ý", "ෂො");
+        text = text.replaceAll("¨|ý", "ඹො");
+        text = text.replaceAll("¨fý", "ඡො");
+        text = text.replaceAll("¨bý", "ඝො");
+        text = text.replaceAll("¨`ý", "ඛො");
+        text = text.replaceAll("¨‡ý", "ළො");
+        text = text.replaceAll("¨ª†ý", "ඟො");
+        text = text.replaceAll("¨oý", "ණො");
+        text = text.replaceAll("¨xý", "ඵො");
+        text = text.replaceAll("¨lý", "ඨො");
+        text = text.replaceAll("¨pý", "ඬො");
+        text = text.replaceAll("¨ƒý", "ශො");
+        text = text.replaceAll("¨jý", "ඥො");
+        text = text.replaceAll("¨vý", "ඳො");
+        text = text.replaceAll("¨„ý", "ෂො");
+        text = text.replaceAll("¨yý", "බො");
+        text = text.replaceAll("¨eý", "චො");
+        text = text.replaceAll("¨mý", "ඩො");
+        text = text.replaceAll("¨‰ý", "ෆො");
+        text = text.replaceAll("¨aý", "ගො");
+        text = text.replaceAll("¨†ý", "හො");
+        text = text.replaceAll("¨gý", "ජො");
+        text = text.replaceAll("¨_ý", "කො");
+        text = text.replaceAll("¨ý", "ලො");
+        text = text.replaceAll("¨\\{ý", "මො");
+        text = text.replaceAll("¨uý", "නො");
+        text = text.replaceAll("¨wý", "පො");
+        text = text.replaceAll("¨sý", "දො");
+        text = text.replaceAll("¨~ý", "රො");
+        text = text.replaceAll("¨…ý", "සො");
+        text = text.replaceAll("¨ký", "ටො");
+        text = text.replaceAll("¨‚ý", "වො");
+        text = text.replaceAll("¨qý", "තො");
+        text = text.replaceAll("¨zý", "භො");
+        text = text.replaceAll("¨}ý", "යො");
+        text = text.replaceAll("¨iý", "ඤො");
+        text = text.replaceAll("¨tý", "ධො");
+        text = text.replaceAll("¨rý", "ථො");
+
+        text = text.replaceAll("¨„‹", "ෂේ"); //There are another 2 combus
+        text = text.replaceAll("¨|", "ඹේ");
+        text = text.replaceAll("¨F", "ඡේ");
+        text = text.replaceAll("¨G", "ඡේ");
+        text = text.replaceAll("¨bŒ", "ඝේ");
+        text = text.replaceAll("¨C", "ඛේ");
+        text = text.replaceAll("¨Œ", "ලේ");
+        text = text.replaceAll("¨‡Œ", "ළේ");
+        text = text.replaceAll("¨ª†Œ", "ඟේ");
+        text = text.replaceAll("¨oŒ", "ණේ");
+        text = text.replaceAll("¨xŒ", "ඵේ");
+        text = text.replaceAll("¨lŒ", "ඨේ");
+        text = text.replaceAll("¨©J", "ඬේ"); //Cant find
+        text = text.replaceAll("¨ªJ", "ඬේ"); //Cant find
+        text = text.replaceAll("¨«J", "ඬේ"); //Cant find
+        text = text.replaceAll("¨ƒŒ", "ශේ");
+        text = text.replaceAll("jŒ", "ඥේ");
+        text = text.replaceAll("¨vŒ", "ඳේ");
+        text = text.replaceAll("¨¬„Œ", "ක්‍ෂේ");
+        text = text.replaceAll("¨O", "බේ");
+        text = text.replaceAll("¨E", "චේ");
+        text = text.replaceAll("¨J", "ඩේ");
+        text = text.replaceAll("¨‰Œ", "ෆේ");
+        text = text.replaceAll("¨aŒ", "ගේ");
+        text = text.replaceAll("¨†Œ", "හේ");
+        text = text.replaceAll("¨G", "ජේ");
+        text = text.replaceAll("¨F", "ඡේ");
+        text = text.replaceAll("¨_Œ", "කේ");
+        text = text.replaceAll("¨P", "මේ");
+        text = text.replaceAll("¨uŒ", "නේ");
+        text = text.replaceAll("¨wŒ", "පේ");
+        text = text.replaceAll("¨sŒ", "දේ");
+        text = text.replaceAll("¨R", "රේ");
+//        text = text.replaceAll("¨R", "රේ");
+        text = text.replaceAll("¨…Œ", "සේ");
+        text = text.replaceAll("¨I", "ටේ");
+        text = text.replaceAll("¨S", "වේ");
+        text = text.replaceAll("¨qŒ", "තේ");
+        text = text.replaceAll("¨zŒ", "භේ");
+        text = text.replaceAll("¨}Œ", "යේ");
+        text = text.replaceAll("¨¬sŒ", "ඤේ");
+        text = text.replaceAll("¨N", "ධේ");
+        text = text.replaceAll("¨rŒ", "ථේ");
+        text = text.replaceAll("¨G", "ජේ");
+        text = text.replaceAll("iŒ", "ඤ්");
+        text = text.replaceAll("iŒ", "ඤ්");
+        text = text.replaceAll("i‹", "ඤ්");
+        text = text.replaceAll("iýý", "ඤාා");
+        text = text.replaceAll("iý", "ඤා");
+        text = text.replaceAll("W•", "ඈ");
+//        text = text.replaceAll("à\\", "ඈ");
+        text = text.replaceAll("¨„", "ෂෙ");
+        text = text.replaceAll("¨|", "ඹෙ");
+        text = text.replaceAll("¨Q", "ඹේ");
+        text = text.replaceAll("¨\\\\", "ඓ"); //double or single?
+        text = text.replaceAll("¨f", "ඡෙ");
+        text = text.replaceAll("¨b", "ඝෙ");
+        text = text.replaceAll("¨`", "ඛෙ");
+        text = text.replaceAll("¨‡", "ළෙ");
+        text = text.replaceAll("¨", "ලෙ");
+        text = text.replaceAll("¨ª†", "ඟෙ");
+        text = text.replaceAll("¨o", "ණෙ");
+        text = text.replaceAll("¨x", "ඵෙ");
+        text = text.replaceAll("¨l", "ඨෙ");
+        text = text.replaceAll("¨p", "ඬෙ");
+        text = text.replaceAll("¨ƒ", "ශෙ");
+        text = text.replaceAll("¨j", "ඥෙ");
+        text = text.replaceAll("¨v", "ඳෙ");
+        text = text.replaceAll("¨¬„", "ක්‍ෂෙ");
+        text = text.replaceAll("¨y", "බෙ");
+        text = text.replaceAll("¨e", "චෙ");
+        text = text.replaceAll("¨m", "ඩෙ");
+        text = text.replaceAll("¨‰", "ෆෙ");
+        text = text.replaceAll("¨a", "ගෙ");
+        text = text.replaceAll("¨†", "හෙ");
+        text = text.replaceAll("¨z", "භෙ");
+        text = text.replaceAll("¨g", "ජෙ");
+        text = text.replaceAll("¨_", "කෙ");
+        text = text.replaceAll("¨\\{", "මෙ");
+        text = text.replaceAll("¨u", "නෙ");
+        text = text.replaceAll("¨w", "පෙ");
+        text = text.replaceAll("¨s", "දෙ");
+        text = text.replaceAll("¨~", "රෙ");
+        text = text.replaceAll("¨…", "සෙ");            //@Gayan
+        text = text.replaceAll("¨k", "ටෙ");
+        text = text.replaceAll("¨‚", "වෙ");
+        text = text.replaceAll("¨q", "තෙ");
+        text = text.replaceAll("¨}", "යෙ");
+        text = text.replaceAll("¨i", "ඤෙ");
+        text = text.replaceAll("¨t", "ධෙ");
+        text = text.replaceAll("¨r", "ථෙ");
+
+        text = text.replaceAll("v", "ඳැ");
+        text = text.replaceAll("W", "ඇ");
+        text = text.replaceAll("ó", "රැ");
+
+        text = text.replaceAll("„§§", "ෂෲ");
+        text = text.replaceAll("ƒ§§", "ශෲ");
+        text = text.replaceAll("`§§", "බෲ");
+        text = text.replaceAll("m§§", "ඩෲ");
+        text = text.replaceAll("‰§§", "ෆෲ");
+        text = text.replaceAll("a§§", "ගෲ");
+        text = text.replaceAll("_§§", "කෲ");
+        text = text.replaceAll("w§§", "පෲ");
+        text = text.replaceAll("…§§", "සෲ");
+        text = text.replaceAll("k§§", "ටෲ");
+        text = text.replaceAll("q§§", "තෲ");
+        text = text.replaceAll("z§§", "භෲ");
+        text = text.replaceAll("t§§", "ධෲ");
+
+        text = text.replaceAll("~•", "රූ");
+//        text = text.replaceAll("~•", "රූ");
+        text = text.replaceAll("q¦", "තූ");
+        text = text.replaceAll("ƒ¦", "ශූ");
+        text = text.replaceAll("_¦", "කූ");
+        text = text.replaceAll("z¦", "භූ");
+        text = text.replaceAll("a¦", "ගූ");
+
+        text = text.replaceAll("©à", "ඳු");
+        text = text.replaceAll("«à", "ඳු");
+        text = text.replaceAll("ªà", "ඳු");
+        text = text.replaceAll("©à", "ඳූ");
+        text = text.replaceAll("«à", "ඳූ");
+        text = text.replaceAll("ªà", "ඳූ");
+
+        text = text.replaceAll("ß", "දු");
+        text = text.replaceAll("à", "දූ");
+
+        text = text.replaceAll("ù", "ලූ");
+
+        text = text.replaceAll("\\[", "ඍ");
+        text = text.replaceAll("Z°", "ඌ");
+        text = text.replaceAll("]°", "ඖ");
+        text = text.replaceAll("Z", "උ");
+        text = text.replaceAll("q¢", "තු");
+        text = text.replaceAll("a¢", "ගු");
+        text = text.replaceAll("_¢", "කු");
+        text = text.replaceAll("z¢", "භු");
+        text = text.replaceAll("~", "රු");
+
+        text = text.replaceAll("ø", "ලු");
+//        text = text.replaceAll("Ï", "ලු");
+        text = text.replaceAll("‚£", "වු");
+        text = text.replaceAll("\\‹", "ඒ");
+        text = text.replaceAll("B", "ඕ");
+        text = text.replaceAll("C", "ඛ්");
+        text = text.replaceAll("E", "ච්");
+        text = text.replaceAll("F", "ඡ්");
+
+        text = text.replaceAll("J", "ඩ්");
+        text = text.replaceAll("ªJ", "ඬ්");
+        text = text.replaceAll("©J", "ඬ්");
+        text = text.replaceAll("«J", "ඬ්");
+
+        text = text.replaceAll("N", "ධ්");
+        text = text.replaceAll("O", "බ්");
+        text = text.replaceAll("P", "ම්");
+        text = text.replaceAll("G", "ජ්");
+        text = text.replaceAll("Q", "ඹ්");
+        text = text.replaceAll("R", "ර්");
+//        text = text.replaceAll("ªJ", "ඬ්");
+        text = text.replaceAll("_Œ", "ක්");
+        text = text.replaceAll("_‹", "ක්");
+        text = text.replaceAll("S", "ව්");
+        text = text.replaceAll("I", "ට්");
+        text = text.replaceAll("¾", "ඛී");
+        text = text.replaceAll("À", "චී");
+
+        text = text.replaceAll("Â", "ඡී");
+        text = text.replaceAll("ð", "ඹී");
+        text = text.replaceAll("Ì", "ටී");
+        text = text.replaceAll("Ð", "ඩී");
+
+        text = text.replaceAll("Ä", "ජී");
+
+
+
+        text = text.replaceAll("«Ð", "ඬී");
+        text = text.replaceAll("æ", "ධී");
+        text = text.replaceAll("ì", "බී");
+        text = text.replaceAll("î", "මී");
+        text = text.replaceAll("ö", "රී");
+        text = text.replaceAll("Þ", "දී");
+        text = text.replaceAll("Y", "ඊ");
+        text = text.replaceAll("û", "වී");
+        text = text.replaceAll("X", "ඉ");
+
+        text = text.replaceAll("½", "ඛි");
+        text = text.replaceAll("¿", "චි");
+        text = text.replaceAll("Á", "ඡි");
+
+        text = text.replaceAll("Ã", "ජි");
+        text = text.replaceAll("Ë", "ටි");
+        text = text.replaceAll("Ï", "ඩි");
+        text = text.replaceAll("å", "ධි");
+        text = text.replaceAll("ë", "බි");
+        text = text.replaceAll("í", "මි");
+        text = text.replaceAll("ï", "ඹි");
+        text = text.replaceAll("õ", "රි");
+        text = text.replaceAll("ú", "වි");
+        text = text.replaceAll("Wý", "ආ");
+        text = text.replaceAll("…", "ස");                //@Gayan     &Ú‘{Ù
+        text = text.replaceAll("‚", "ව");
+        text = text.replaceAll("‰", "ෆ");
+        text = text.replaceAll("ªx", "ළු");
+        text = text.replaceAll("†", "හ");
+        text = text.replaceAll("‡", "ළ");
+        text = text.replaceAll("¬„", "ක්‍ෂ");
+        text = text.replaceAll("\\{", "ම");
+        text = text.replaceAll("ƒ", "ශ");
+        text = text.replaceAll("„", "ෂ");
+        text = text.replaceAll("|", "ඹ");
+        text = text.replaceAll("}", "ය");
+        text = text.replaceAll("~", "ර");
+        text = text.replaceAll("", "ල");
+        text = text.replaceAll("W", "අ");
+        text = text.replaceAll("\\\\", "එ");
+                text = text.replaceAll("]", "ඔ");
+        text = text.replaceAll("_", "ක");
+        text = text.replaceAll("`", "ඛ");
+        text = text.replaceAll("a", "ග");
+        text = text.replaceAll("b", "ඝ");
+        text = text.replaceAll("ª†", "ඟ");
+        text = text.replaceAll("e", "ච");
+        text = text.replaceAll("f", "ඡ");
+        text = text.replaceAll("g", "ජ");
+        text = text.replaceAll("k", "ට");
+        text = text.replaceAll("l", "ඨ");
+        text = text.replaceAll("m", "ඩ");
+        text = text.replaceAll("o", "ණ");
+        text = text.replaceAll("p", "ඬ");
+        text = text.replaceAll("q", "ත");
+        text = text.replaceAll("r", "ථ");
+        text = text.replaceAll("t", "ධ");
+        text = text.replaceAll("u", "න");
+        text = text.replaceAll("w", "ප");
+        text = text.replaceAll("\\\\", "එ");
+                text = text.replaceAll("y", "බ");
+        text = text.replaceAll("z", "භ");
+        text = text.replaceAll("\\{", "ම");
+        text = text.replaceAll("s", "ද");
+        text = text.replaceAll("v", "ඳ");
+        text = text.replaceAll("j", "ඥ");
+        text = text.replaceAll("â", "ද්‍ර");
+
+        text = text.replaceAll("", "ැ");
+        text = text.replaceAll("•", "ෑ");
+        text = text.replaceAll("Ÿ", "ු");
+        text = text.replaceAll("£", "ූ");
+        text = text.replaceAll("þ", "ූ");
+        text = text.replaceAll("¡¡", "ූ");
+        text = text.replaceAll("¤", "ූ");
+
+        text = text.replaceAll(" š","ි");
+        text = text.replaceAll(" ˜","ි");
+        text = text.replaceAll(" ™","ි");
+
+        text = text.replaceAll(" œ", "ී");
+        text = text.replaceAll(" ›", "ී");
+        text = text.replaceAll("§", "ෘ");
+        text = text.replaceAll("°", "ෟ");
+        text = text.replaceAll("ý", "ා");
+
+        text = text.replaceAll(" ‹", "්");
+        text = text.replaceAll(" Œ", "්");
+        text = text.replaceAll("Š", "ං");
+        text = text.replaceAll("¨", "ෙ");    // this should be avoided, but no choice due to the font mixing      @Gayan
+        text = text.replaceAll("s", "දැ");    // This is a common error   @Gayan
+
+
+        return text;
+    }
+
+    public static boolean lastCharError(String text) {
+        if (text.endsWith("¨")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String fixLastCharError(String text) {
+        return "¨" + text;
+    }
+
+
+}
+
