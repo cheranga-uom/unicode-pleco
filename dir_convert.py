def87bbb5d6a3ca61dec64f2343274f451062ea5
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2019-05-04 13:09:07 +0530

    Change script path to online

--- dir_convert.py
+++ dir_convert.py
@@ -3,7 +3,8 @@ import json
 import os
 
 apikey="abcd1234"
-
+api_upload_path="https://pleco.uom.lk/api/upload"
+api_download_path="https://pleco.uom.lk/api/download?conversionId="
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 try:
 	os.mkdir("converted")
@@ -12,12 +13,12 @@ except FileExistsError as e:
 for fname in files:
     print(fname)
     with open(fname, 'rb') as f:
-        r = requests.post('http://localhost:8080/api/upload', headers={"apikey":apikey}, files={'file': f},data={"inputfiletype":"officeXML"})
+        r = requests.post(api_upload_path, headers={"apikey":apikey}, files={'file': f},data={"inputfiletype":"officeXML"})
         print(r)
         rj = json.loads(r.content)
         if(rj["status"]=="success"):
-            print('Beginning file download with requests')
-            url = "http://localhost:8080/api/download?conversionId=" + str(rj["conversionId"])
+            print('Downloading converted file'+fname)
+            url = api_download_path + str(rj["conversionId"])
             download_response = requests.get(url, headers={"apikey":apikey} )
             # dr_headers = json.loads(download_response.headers)
             conv_file_name=('./converted/converted-'+fname)
